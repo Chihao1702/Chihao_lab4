6453f256729d311c97529453884b226d8db42367
Chihao1702 <117892988+Chihao1702@users.noreply.github.com>
2023-03-10 23:42:14 +0700

    Create thunghiem.java

---
+++ thunghiem.java
@@ -0,0 +1,11 @@
+package LAB04;
+
+import LAB04.DanhSachSinhVien;
+public class test {
+    public static void main (String[] arg)
+    {
+    DanhSachSinhVien dssv = new DanhSachSinhVien();
+    dssv.menu();
+    
+    }
+}
